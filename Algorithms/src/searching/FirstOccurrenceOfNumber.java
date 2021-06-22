1da15786e0d84924f6005b66d931e9b36748cac6
NirajThagunna <nirajthagunna123@gmail.com>
2021-06-22 10:11:50 +0545

    First and Last Occurrence of an element

---
+++ Algorithms/src/searching/FirstOccurrenceOfNumber.java
@@ -0,0 +1,75 @@
+package searching;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import fastIO.ArrayIO;
+import fastIO.BasicIO;
+
+// Program to find the first occurrence of a number using Binary Search Algorithm
+public class FirstOccurrenceOfNumber {
+
+	// method to find the first occurrence of a number
+	public static int binarySearch(int[] arr, int data) {
+
+		int low = 0, high = arr.length - 1, pos = -1;
+
+		while (low <= high) {
+
+			int mid = low + (high - low) / 2;
+
+			if (data == arr[mid]) {
+				pos = mid;
+				high = mid - 1;
+			}
+			else if (data < arr[mid]) {
+				high = mid - 1;
+			}
+			else {
+				low = mid + 1;
+			}
+		}
+
+		// if the data element is not present in an array, then return -1
+		return pos;
+	}
+
+	// main() method
+	public static void main(String[] args) {
+		try {
+			System.out.println("******* Binary Search Algorithm - First Occurrence Of an Element *********");
+			System.out.print("How many elements do you want to store? ");
+			int MAX = BasicIO.nextInt();
+
+			// Creating an array
+			int[] arr = ArrayIO.readArray(MAX);
+
+			// printing an array
+			System.out.println("\nElements of an array are : ");
+			ArrayIO.printArray(arr);
+
+			// Data element to be searched
+			System.out.print("\nEnter the element whose first occurrence do you want to find : ");
+			int data = BasicIO.nextInt();
+
+			// binarySearch()
+			int result = binarySearch(arr, data);
+			if (result == -1) 
+				System.out.println("\n" + data + " is not found!");
+			else 
+				System.out.println("\nThe first occurrence of " + data + " is at position " + (result + 1));
+		}
+		catch (Exception ex) {
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			ex.printStackTrace(pw);
+			String error = sw.toString();
+			System.out.println("Error : \n" + error);
+		}
+	}
+}
+
+
+
+
+
