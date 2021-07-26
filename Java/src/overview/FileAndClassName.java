cf2bed9eca525aac56dc984611b71d6c7d7fe788
Niraj Thagunna <nirajthagunna123@gmail.com>
2021-07-26 18:06:56 +0545

    Into to Java

---
+++ Java/src/overview/FileAndClassName.java
@@ -0,0 +1,10 @@
+package overview;
+
+// Java program to demonstrate the difference between the java file name and class name
+public class FileAndClassName {
+
+	public static void main(String[] args) {
+		
+		System.out.println("File name and Class name should be same!");
+	}
+}
---
+++ Java/src/overview/IsMainComOrNot.java
@@ -0,0 +1,10 @@
+package overview;
+
+// Java program to check whether the main method is compulsory or not?
+public class IsMainComOrNot {
+
+	// static block
+	static {
+		System.out.println("Hello Java World!");
+	}
+}
