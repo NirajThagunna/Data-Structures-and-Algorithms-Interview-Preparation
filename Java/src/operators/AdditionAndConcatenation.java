6cefe8c73c49dc5c6cfdb5963b8bce865167e5b5
Niraj Thagunna <nirajthagunna123@gmail.com>
2021-08-01 08:22:08 +0545

    Command Line Arguments

---
+++ Java/src/operators/AdditionAndConcatenation.java
@@ -0,0 +1,11 @@
+package operators;
+
+// Java program to illustrate the concept of Addition and Concatenation
+public class AdditionAndConcatenation {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1 + 2 + 3 + "Niraj Thagunna" + 1 + 2 + 3);
+		System.out.println('a' + 'b' + "Niraj" + (1 + 2 + 5));
+	}
+}
